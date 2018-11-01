e686125f0c31c2d71e84ec6865db9baee303d2a3
Carlos Mercado <carlos.mercado@3creatives.com.co>
2018-11-01 18:54:21 -0500

    firts commits

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor( private http:HttpClient ) {
+    console.log
+  }
+
+  getNewReleases(){
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQCtPrY7XV1NgePzYomhLEzCXtMl76QJQ93fL4BPlaAJStofDswxfJVndcSBgO8FOrrb59BF9clDjmvDb6U'
+    })
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers})
+        .subscribe( data => {
+          console.log(data)
+        }
+    )
+  }
+
+
+}
